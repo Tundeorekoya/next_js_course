aafd82a83c1cd62f13fa4b6b6d9ec963f82e12aa
tundeorekoya <tundeorekoya1998@gmail.com>
2024-04-20 08:13:15 -0700

    update

---
+++ app/f1/(.)f2/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <h1>
+        interaction f2
+    </h1>
+  )
+}
---
+++ app/f1/f2/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const F2 = () => {
+  return (
+    <h1>
+      f2 page
+    </h1>
+  )
+}
+
+export default F2
